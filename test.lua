4ca31d826600b03782a9a27516243b75f3c61da0
standzzz <stands903@gmail.com>
2025-12-15 00:28:28 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,6 @@
 -- // Introduce the API 
 
-script_key = "FbUSUBSpwgEkxACkGrMoGzglwZROtjhz";
+script_key = "CLODDSlqJfmwQrjuIXqYaJHyLyzfKPmc";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d836cd48a6355851cfebe7811b9ad445.lua"))()
 d2c = getgenv().d2c
 
